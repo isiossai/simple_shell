dd08b13daa73ab4204d571db9bc6930a715cda73
isiossai <isiomaossai@icloud.com>
2022-08-10 13:31:54 +0100

    path

--- path.c
+++ path.c
@@ -100,7 +100,7 @@ void check_for_path(vars_t *vars)
 				new_exit(vars);
 			}
 		}
-	           (path == NULL || path_tokens[i] == NULL);
+	          if(path == NULL || path_tokens[i] == NULL)
 		{
 			print_error(vars, ": not found\n");
 			vars->status = 127;
